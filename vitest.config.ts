b7f84212a932679bd9f0ef0a065d7a70418823d0
snowfall <ammm101046@gmail.com>
2026-02-24 17:09:28 +0300

    chore(vitest): implement src initial path

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     globals: true,
     setupFiles: "./vitest.setup.ts",
     alias: {
-      "@": path.resolve(__dirname, "./"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
