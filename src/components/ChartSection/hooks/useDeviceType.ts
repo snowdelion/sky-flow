1988253379762216181434f4708c119d347a99c8
snowfall <ammm101046@gmail.com>
2026-03-05 21:41:56 +0300

    fix(hooks): implement minimum width to 1024px for desk and smallDesk

--- src/components/ChartSection/hooks/useDeviceType.ts
+++ src/components/ChartSection/hooks/useDeviceType.ts
@@ -4,8 +4,8 @@ import { useMediaQuery } from "react-responsive";
 export function useDeviceType(): UseDeviceTypeReturn {
   const isMobile = useMediaQuery({ maxWidth: 640 });
   const isTablet = useMediaQuery({ minWidth: 641, maxWidth: 768 });
-  const isDesk = useMediaQuery({ minWidth: 1025 });
-  const isSmallDesk = useMediaQuery({ minWidth: 1025, maxWidth: 1150 });
+  const isDesk = useMediaQuery({ minWidth: 1024 });
+  const isSmallDesk = useMediaQuery({ minWidth: 1024, maxWidth: 1150 });
 
   return useMemo(
     () => ({
