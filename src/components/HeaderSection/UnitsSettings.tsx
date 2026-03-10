a461d091c59ff5ee1fe0b97fc551bb6906efe746
snowfall <ammm101046@gmail.com>
2026-03-10 18:30:35 +0300

    refactor(units): merge MENU_OPTIONS into main file

--- src/components/HeaderSection/UnitsSettings.tsx
+++ src/components/HeaderSection/UnitsSettings.tsx
@@ -7,8 +7,7 @@ import checkmarkIcon from "@/../public/icons/icon-checkmark.svg";
 import dropdownIcon from "@/../public/icons/icon-dropdown.svg";
 import unitsIcon from "@/../public/icons/icon-units.svg";
 import { useSettingsStore } from "@/stores/useSettingsStore";
-
-import { MENU_OPTIONS } from "./UnitsSettings.constants";
+import type { WeatherDataUnits } from "@/types/api/WeatherData";
 
 export default function UnitsSettings() {
   const units = useSettingsStore((state) => state.units);
@@ -80,3 +79,42 @@ export default function UnitsSettings() {
     </Menu>
   );
 }
+
+const MENU_OPTIONS = [
+  {
+    id: 1,
+    title: "Temperature",
+    unit: "temperature" as keyof WeatherDataUnits,
+    options: [
+      { label: "Celsius (°C)", value: "celsius" },
+      { label: "Fahrenheit (°F)", value: "fahrenheit" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Wind Speed",
+    unit: "speed" as keyof WeatherDataUnits,
+    options: [
+      { label: "Kilometers (km)", value: "kmh" },
+      { label: "Miles (mi)", value: "mph" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Precipitation",
+    unit: "precipitation" as keyof WeatherDataUnits,
+    options: [
+      { label: "Millimeters (mm)", value: "mm" },
+      { label: "Inches (in)", value: "inch" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Time Format",
+    unit: "time" as keyof WeatherDataUnits,
+    options: [
+      { label: "12-hour", value: "12" },
+      { label: "24-hour", value: "24" },
+    ],
+  },
+];
